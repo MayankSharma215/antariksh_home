f4d0f0f7e359c1d08102c5c2a7c7241ba4a1fcab
MayankSharma215 <bittumayank2004@gmail.com>
2023-08-13 23:30:56 +0530

    Intial Commit

---
+++ src/Compnents/WhatWeDo/WhatWeDo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './WhatWeDo.css';
+
+const WhatWeDo = () => {
+    return ( 
+      <div className="whatWeDo">
+        <h1>Antariskh Stands For</h1>
+      </div>
+     );
+}
+ 
+export default WhatWeDo;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import Header from './Compnents/Header/Header';
+import Intro from './Compnents/Intro/Intro';
+import News from './Compnents/News/News';
+import Footer from './Compnents/Footer/Footer';
+// import WhatWeDo from './Compnents/WhatWeDo/WhatWeDo';
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <Intro/>
+     <News/>
+     <Footer/>
+     {/* <WhatWeDo/> */}
+    </>
+  );
+}
+
+export default Home;
